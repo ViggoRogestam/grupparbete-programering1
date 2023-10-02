7f29b69e01bedb6a9f940fad63fda983c11ce8fe
Viggo Rogestam <viggo.rogestam@protonmail.com>
2023-10-02 11:23:14 +0200

    skapade modul för enhetsomvandling och ui

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import urllib.request
 
-url = 'https://github.com/ViggoRogestam/grupparbete-programering1'
-filename = 'my_module.py'
+url = 'https://raw.githubusercontent.com/ViggoRogestam/grupparbete-programering1/main/Lenght_converter.py'
+filename = 'Lenght_converter.py'
 urllib.request.urlretrieve(url, filename)
 
-from my_module import my_function
+from Lenght_converter import Length
+from Lenght_converter import celsius_to_fahrenheit
+from Lenght_converter import fahrenheit_to_celsius
+
+url = 'https://raw.githubusercontent.com/ViggoRogestam/grupparbete-programering1/main/ui.py'
+filename = 'ui.py'
+urllib.request.urlretrieve(url, filename)
+
+from ui import Ui
+
+ui = Ui(60)
+ui.print_header('Rubrik')
+ui.print_line()
+ui.print_choices('test1','test2', 'test3')
+ui.print_dot()
