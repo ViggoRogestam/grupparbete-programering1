65ed6c07775961be69e4f03e4628e9c057a23d1c
Viggo Rogestam <viggo.rogestam@protonmail.com>
2023-10-03 17:42:12 +0200

    gjorde todo på Lenght_converter samt fixade en bugg i Lenght_converter.py

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ while True:
     choice = input('Enter choice >')
     # TODO: ge error vid felaktig inmatning:
     if choice == '1':
+        clear_screen()
         print_menu()
         choice = input('Enter choice >')
         calculate_volume(choice, volume_calculator)
@@ -32,17 +33,48 @@ while True:
                 val = input("Välj ett av alternativen ovan genom att ange dess siffra!\n> ")
                 if not calculate_volume(val, volume_calculator):
                     break
-    # TODO: fixa ui, låt andändaren välja vad längden ska konverteras till
     elif choice == '2':
+        clear_screen()
+        ui.print_header('Lenght Converter')
+        ui.print_line()
         length = input('Enter lenght and unit >')
         value, unit = length.split()
         float(value)
         str(unit)
-        l1 = Lenght_converter.Length(value, unit)
-        print(l1.to_kilometer())
-        input()
+        l1 = Length(value, unit)
+        # Ui
+        clear_screen()
+        ui.print_header('Choose unit to convert to')
+        ui.print_line()
+        ui.print_choices('Milimeter', 'Centimeter', 'Meter', 'Kilometer', 'Inches', 'Feet')
+        ui.print_dot()
+        choice = input('>')
+        ui.print_line()
+        #  if milimeter
+        if choice == '1':
+            print(f'{length} equals {(l1.to_milimeter())} milimeter')
+            input()
+        elif choice == '2':
+            print(f'{length} equals {(l1.to_centimeter())} centimeter')
+            input()
+        elif choice == '3':
+            print(f'{length} equals {(l1.to_meter())} meter')
+            input()
+        elif choice == '4':
+            print(f'{length} equals {(l1.to_kilometer())} kilometer')
+            input()
+        elif choice == '5':
+            print(f'{length} equals {(l1.to_inches())} inches')
+            input()
+        elif choice == '6':
+            print(f'{length} equals {(l1.to_feet())} feet')
+            input()
+        else:
+            input('Error: no valid input.\n Press Enter to continue...')
+
     # TODO: obejkt-orientera koden sedan importera den:
     elif choice == '3':
+        clear_screen()
         pass
     else:
         input('\nNo valid choice. Press enter to continue...')
